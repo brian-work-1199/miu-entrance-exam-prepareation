ed32a2f1e08722f7cf03940442ecaa94a99a8ddb
Brian Thinh Le <thinh.le.tpv@one-line.com>
2023-09-19 16:35:34 +0700

    commit

--- solution/decodeArrayy.java
+++ solution/decodeArrayy.java
@@ -8,8 +8,6 @@ public class decodeArrayy {
         for(i=0;i<a.length;i++){
             if(a[i] == 0){
                 match++;
-            }else if(match == 0 && a[i] == 1){
-                sum = sum*10 + match;
             }else{
                 sum = sum*10 + match;
                 match = 0;
@@ -18,6 +16,7 @@ public class decodeArrayy {
         return a[0] < 0 ? -sum:sum;
     }
     public static void main(String[] args){
+        System.out.println(f(new int[]{0,0,0,1,0,0,0,0,1}));
         System.out.println(f(new int[]{1}));
         System.out.println(f(new int[]{0,1}));
         System.out.println(f(new int[]{-1,0,1}));
