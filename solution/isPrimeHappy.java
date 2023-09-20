43d37ef1d5a2962df07b8d3ddd267fc6975e85c5
Brian Thinh Le <thinh.le.tpv@one-line.com>
2023-09-20 21:16:58 +0700

    commit

---
+++ solution/isPrimeHappy.java
@@ -0,0 +1,28 @@
+package solution;
+
+public class isPrimeHappy {
+
+    static int f(int n){
+        int sum = 0, i = 0, j = 0;
+        for (i = 2; i < n; i++){
+            boolean isPrime = true;
+            for (j = 2; j < i; j++){
+                if(i%j==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                sum += i;
+            }
+        }
+        return (sum%n==0 && n > 2) ? 1:0;
+    }
+    public static void main(String[] args){
+        System.out.println(f(5));
+        System.out.println(f(25));
+        System.out.println(f(32));
+        System.out.println(f(8));
+        System.out.println(f(2));
+    }
+}
